1b473d7cc4057bdd0f072dd789538135f10f263b
Hassan <malik.hr.2004@gmail.com>
2026-03-25 11:03:24 +0500

    Add SecondGenExample class to demonstrate generics in Java

--- Generics/SecondGenExample.java
+++ Generics/SecondGenExample.java
@@ -1,3 +1,23 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class SecondGenExample {
-  
+  public static void main(String args[]) {
+    List list = new ArrayList<>();
+    list.add("Developer");
+
+    // String str1 = list.get(0); cannot work because compiler does not know the type of list index
+    String str2 = (String) list.get(0);
+    System.out.println(str2);
+
+
+    List<String> li = new ArrayList<>();
+    li.add("MERN Developer");
+    li.add("Backend Developer");
+    String str3 = li.get(0);
+    String str4 = li.get(1);
+
+    System.out.println(str3);
+    System.out.println(str4);
+  }
 }

0c38ac27503c3ff7cab2922e9f76bf23c8af8dba
Hassan <malik.hr.2004@gmail.com>
2026-03-25 12:26:08 +0500

    Add ArrayListExample2 class to demonstrate basic operations with ArrayList in Java

---
+++ Collections/ArrayListExample2.java
@@ -0,0 +1,18 @@
+package Collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListExample2 {
+  public static void main(String args[]) {
+    List<Integer> l = new ArrayList<>();
+    l.add(1);
+    l.add(2);
+    l.add(3);
+    System.out.println(l);
+    l.remove(0);
+    System.out.println(l);
+    l.clear();
+    System.out.println(l);
+  }
+}

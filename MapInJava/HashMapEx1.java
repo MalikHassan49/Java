eedc8e52bb2e67b4ed04c4391a0532eeb5a066cf
Hassan <malik.hr.2004@gmail.com>
2026-03-25 15:14:00 +0500

    Add HashMapEx1 class to demonstrate basic usage of HashMap in Java

---
+++ MapInJava/HashMapEx1.java
@@ -0,0 +1,12 @@
+package MapInJava;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapEx1 {
+  public static void main(String args[]) {
+    Map<String, String> map = new HashMap<>();
+    map.put("MyName: " , "Ali");
+    System.out.println(map);
+  }
+}
